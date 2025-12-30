4cd8a29d0cb67e670a2a731adb127cd8e4b8d68c
rubtura <62082614+rubtura@users.noreply.github.com>
2025-12-29 21:16:46 -0400

    Update astro.config.mjs with site and base
    Added site and base configuration for deployment.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,15 @@
-import { defineConfig } from 'astro/config';
-import react from '@astrojs/react';
-import tailwind from '@astrojs/tailwind';
+import { defineConfig } from "astro/config";
+import react from "@astrojs/react";
+import tailwind from "@astrojs/tailwind";
 
 export default defineConfig({
+  site: "https://rubtura.github.io",
+  base: "/qhatu-project",
+
   integrations: [
     react(),
     tailwind({
       applyBaseStyles: false,
-    })
+    }),
   ],
 });
